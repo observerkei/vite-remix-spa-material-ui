55586eb2c47f35c3b5a4f35af9d4dce53be74505
observerkei <observerkei@foxmail.com>
2024-12-20 18:24:21 +0800

    refactor: set Container to full height

--- app/mui/Layout.tsx
+++ app/mui/Layout.tsx
@@ -6,10 +6,24 @@ import Copyright from './Copyright';
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <Container maxWidth="xl" disableGutters>
+    <Container
+      maxWidth={false}
+      disableGutters
+      sx={{
+        height: '100vh',
+        display: 'flex',
+        alignItems: 'stretch',
+      }}
+    >
+      <Box sx={{
+        flexGrow: 1,
+        display: 'flex',
+        flexDirection: 'column',
+      }}>
         {children}
         <ProTip />
         <Copyright />
+      </Box>
     </Container>
   );
 }
